34ff49f44289b3fba4ee73a193e2efe59f17dff3
Pablo Klaschka <contact@pabloklaschka.de>
2019-10-23 14:47:12 +0200

    Adjusted/simplified theme

--- src/theme.js
+++ src/theme.js
@@ -1,20 +1,18 @@
-import { red } from '@material-ui/core/colors';
-import { createMuiTheme } from '@material-ui/core/styles';
+import {amber, blueGrey, red} from '@material-ui/core/colors';
+import {createMuiTheme} from '@material-ui/core/styles';
 
 const theme = createMuiTheme({
     palette: {
         primary: {
-            main: '#546e7a'
+            main: blueGrey["700"],
+            dark: blueGrey['500']
         },
         secondary: {
-            main: '#ffca28'
+            main: amber['500']
         },
         error: {
             main: red.A400
         },
-        background: {
-            default: '#fff'
-        }
     }
 });
 
