544963de05f60ebb40a8c12767b645c6250f891b
Ludwig Richter <richter@fliegwerk.com>
2019-11-01 17:08:50 +0100

    Test raw pretty output overflow

--- src/config/component-registry.js
+++ src/config/component-registry.js
@@ -1,8 +1,10 @@
 import Raw from '../components/front-end-modules/raw';
+import RawPretty from '../components/front-end-modules/raw-pretty';
 import Systemd from '../components/front-end-modules/systemd';
 
 const components = {
     raw: Raw,
+    rawPretty: RawPretty,
     systemd: Systemd
 };
 
