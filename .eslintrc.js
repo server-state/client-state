21bdd0c48bf7525bb28697946abbb0decbf79f3b
Ludwig <richter@fliegwerk.com>
2020-07-05 11:18:40 +0200

    Added prettier and eslint lint and fix (2)

--- .eslintrc.js
+++ .eslintrc.js
@@ -20,7 +20,11 @@ module.exports = {
         'plugin:jest/recommended'
     ],
     rules: {
-        'eslint-comments/no-unused-disable': 'error'
+        'eslint-comments/no-unused-disable': 'error',
+        '@typescript-eslint/explicit-function-return-type': 'off',
+        '@typescript-eslint/explicit-module-boundary-types': 'off',
+        '@typescript-eslint/no-var-requires': 'off',
+        'no-param-reassign': ['error', { props: false }]
     },
     parserOptions: {
         ecmaFeatures: {
@@ -29,13 +33,7 @@ module.exports = {
     },
     settings: {
         react: {
-          createClass: 'createReactClass', // Regex for Component Factory to use,
-                                             // default to "createReactClass"
-          pragma: 'React',  // Pragma to use, default to "React"
-          version: 'detect' // React version. "detect" automatically picks the version you have installed.
-                            // You can also use `16.0`, `16.3`, etc, if you want to override the detected value.
-                            // default to latest and warns if missing
-                            // It will default to "detect" in the future
+          version: 'detect'
         }
     }
 };
